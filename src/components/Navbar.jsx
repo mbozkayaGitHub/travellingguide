79988bc4f10f41f1a909e5f8309edc966c82ba53
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-17 22:42:20 +0300

    Clear

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1 className='display-5 text-danger'>TOP 10 CITIES TO TRAVEL IN TURKIYE</h1>
+    </div>
+  )
+}
+
+export default Navbar
